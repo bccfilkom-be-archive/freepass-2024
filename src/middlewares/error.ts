5ae524c58c720409c51f4835592b141b514d82d3
ahargunyllib <billy.bpm03@gmail.com>
2024-02-06 20:45:20 +0700

    update endpoint error handler

--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -2,20 +2,20 @@ import { type Request, type Response, type NextFunction } from 'express'
 import { logger } from '../utils/logger'
 
 export const unknownEndpoint = (request: Request, response: Response) => {
-  response.status(404).send({ status: 400, error: 'unknown endpoint' })
+  response.status(404).send({ status: 404, message: 'unknown endpoint' })
 }
 
 export const errorHandlerEndpoint = (error: Error, request: Request, response: Response, next: NextFunction) => {
   logger.error(error.message)
 
   if (error.name === 'CastError') {
-    return response.status(400).send({ status: 400, error: 'malformatted id' })
+    return response.status(400).send({ status: 400, message: 'malformatted id' })
   } else if (error.name === 'ValidationError') {
-    return response.status(400).json({ status: 400, error: error.message })
+    return response.status(400).json({ status: 400, message: error.message })
   } else if (error.name === 'JsonWebTokenError') {
-    return response.status(401).json({ status: 401, error: 'invalid token' })
+    return response.status(401).json({ status: 401, message: 'invalid token' })
   } else if (error.name === 'TokenExpiredError') {
-    return response.status(401).json({ status: 401, error: 'token expired' })
+    return response.status(401).json({ status: 401, message: 'token expired' })
   } else {
     next(error)
   }
