181529bde4c1f54f0f56541eefe430e5e420d5df
ahargunyllib <billy.bpm03@gmail.com>
2024-02-07 20:57:39 +0700

    refactor auth controller

--- src/controller/auth.controller.ts
+++ src/controller/auth.controller.ts
@@ -1,64 +1,63 @@
-import type { Request, Response } from 'express'
+import type { NextFunction, Request, Response } from 'express'
 import { createUserValidation, loginValidation } from '../validation/auth.validation'
 import type { RegisterForm, LoginForm } from '../types/auth.type'
 import { logger } from '../utils/logger'
 import { createUser, findUserByUsername } from '../services/auth.service'
 import { checkPassword, hashing } from '../utils/bcrypt'
 import { signJWT } from '../utils/jwt'
+import { config } from '../utils/config'
+import { findUserByField } from '../services/user.service'
 
-export const register = async (req: Request, res: Response) => {
-  const payload: RegisterForm = req.body
-  const { error, value } = createUserValidation(payload)
-  if (error) {
-    logger.error(`auth - register - validation: ${error.details[0].message.replace(/"/g, '')}`)
-    return res
-      .status(400)
-      .send({ status: 400, message: `create user failed. ${error.details[0].message.replace(/"/g, '')}` })
-  }
-
+export const register = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const payload: RegisterForm = value
+    const payload: RegisterForm = req.body
+
+    const { error } = createUserValidation(payload)
+    if (error) throw error
+
+    let foundUser: boolean = false
+    if (payload.nim) foundUser = !!(await findUserByField('nim', payload.nim))
+    if (foundUser) throw Error('nim has been taken')
+
+    if (payload.username) foundUser = !!(await findUserByField('username', payload.username))
+    if (foundUser) throw Error('username has been taken')
+
+    if (payload.email) foundUser = !!(await findUserByField('email', payload.email))
+    if (foundUser) throw new Error('email has been taken')
+
     payload.password = hashing(payload.password)
     const user = await createUser(payload)
     return res.status(201).send({ status: 201, message: `create user success. please login ${user.username}` })
   } catch (error: any) {
-    const taken: object = error.keyValue
-    logger.error(`auth - register - saving to db: ${Object.keys(taken)[0]} has been taken`)
-    return res.status(400).send({ status: 400, message: `create user failed. ${Object.keys(taken)[0]} has been taken` })
+    if (error.message.includes('has been taken')) {
+      logger.error('user - update - searching in db: ', error.message)
+      res.status(400).send({ status: 400, message: error.message })
+    } else {
+      next(error)
+    }
   }
 }
 
-export const login = async (req: Request, res: Response) => {
-  const payload: LoginForm = req.body
-  const { error, value } = loginValidation(payload)
-  if (error) {
-    logger.error(`auth - login - validation: ${error.details[0].message.replace(/"/g, '')}`)
-    return res.status(400).send({ status: 400, message: `login failed. ${error.details[0].message.replace(/"/g, '')}` })
-  }
-
+export const login = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    const payload: LoginForm = value
+    const payload: LoginForm = req.body
+    const { error } = loginValidation(payload)
+    if (error) throw error
+
     const user = await findUserByUsername(payload.username)
-    if (!user) {
-      logger.error(`auth - login - searching in db: ${payload.username} not found`)
-      return res.status(401).send({ status: 401, message: `login failed. ${payload.username} not found` })
-    }
+    if (!user) throw Error('username is wrong')
 
     const isValid = checkPassword(payload.password, user.password)
-    if (!isValid) {
-      logger.error('auth - login - searching in db: password wrong')
-      return res.status(401).send({ status: 401, message: 'login failed. password is wrong' })
-    }
+    if (!isValid) throw Error('password is wrong')
 
-    const token = signJWT({ ...user }, { expiresIn: '30s' })
-    if (!token) {
-      logger.error('auth - login - creating token')
-      return res.status(500).send({ status: 500, message: 'internal server error' })
-    }
-
+    const token = signJWT({ ...user }, { expiresIn: config.jwtAccessExpiration })
     return res.status(200).send({ status: 200, message: 'login success', data: token })
   } catch (error: any) {
-    logger.error(`auth - login - searching in db: ${error}`)
-    return res.status(500).send({ status: 500, message: `login failed. ${error}` })
+    if (error.message.includes('is wrong')) {
+      logger.error('user - update - searching in db: ', error.message)
+      res.status(400).send({ status: 400, message: error.message })
+    } else {
+      next(error)
+    }
   }
 }
--- src/utils/config.ts
+++ src/utils/config.ts
@@ -4,9 +4,11 @@ dotenv.config()
 const PORT: number = Number(process.env.PORT) ?? 4000
 const MONGODB_URI = process.env.MONGODB_URI ?? 'MongoDB URI not found'
 const JWT_SECRET = process.env.JWT_SECRET ?? 'thisisverysecret'
+const JWT_ACCESS_EXPIRATION = process.env.JWT_ACCESS_EXPIRATION
 
 export const config = {
   port: PORT,
   mongodbUri: MONGODB_URI,
-  jwtSecret: JWT_SECRET
+  jwtSecret: JWT_SECRET,
+  jwtAccessExpiration: JWT_ACCESS_EXPIRATION
 }
