01050a637af4d0185a1584e251f1231c4a0f54cf
ahargunyllib <billy.bpm03@gmail.com>
2024-02-08 06:20:29 +0700

    adding view user by id

--- src/models/user.model.ts
+++ src/models/user.model.ts
@@ -63,7 +63,7 @@ userSchema.set('toJSON', {
     returnedObject.id = returnedObject._id.toString()
     delete returnedObject._id
     delete returnedObject.__v
-    delete returnedObject.passwordHash
+    delete returnedObject.password
   }
 })
 
