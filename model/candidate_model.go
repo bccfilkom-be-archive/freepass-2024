e405203f26c3fa80e3676ee26e255f4ab3b210dc
nathakusuma <nathakusuma07@gmail.com>
2024-02-10 18:46:55 +0800

    fix bug in promote candidate feature

--- model/candidate_model.go
+++ model/candidate_model.go
@@ -1,5 +1,9 @@
 package model
 
+type PromoteCandidateResponse struct {
+	ID uint `json:"candidate_id"`
+}
+
 type GetCandidateResponse struct {
 	ID     uint   `json:"id"`
 	UserID uint   `json:"user_id"`
