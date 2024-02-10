5bb64b25e76d0b10a7e93918ee4eeb28e6e9005e
nathakusuma <nathakusuma07@gmail.com>
2024-02-10 16:20:25 +0800

    add url query binding helper functions

---
+++ util/binding/bind_query.go
@@ -0,0 +1,38 @@
+package binding
+
+import (
+	"bcc-be-freepass-2024/util/errortypes"
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"strconv"
+)
+
+func ShouldQueryExist(ctx *gin.Context, query string) (string, *errortypes.ApiError) {
+	value, isExist := ctx.GetQuery(query)
+	if !isExist {
+		return "", &errortypes.ApiError{
+			Code:    http.StatusBadRequest,
+			Message: "missing query: " + query,
+			Data:    gin.H{},
+		}
+	}
+	return value, nil
+}
+
+func ShouldUintQuery(ctx *gin.Context, query string) (uint, *errortypes.ApiError) {
+	valueStr, err := ShouldQueryExist(ctx, query)
+	if err != nil {
+		return 0, err
+	}
+
+	value, err2 := strconv.ParseUint(valueStr, 10, 64)
+	if err2 != nil {
+		return 0, &errortypes.ApiError{
+			Code:    http.StatusBadRequest,
+			Message: "invalid query: " + query,
+			Data:    gin.H{},
+		}
+	}
+
+	return uint(value), nil
+}
